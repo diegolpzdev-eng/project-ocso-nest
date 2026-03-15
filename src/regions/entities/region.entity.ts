3fa0a9edd0b609a4fb1b368982d478781442a5da
Luis Diego Hernandez Lopez <diegolpz.dev@gmail.com>
2026-03-14 23:27:03 -0600

    Manager and Regions setup

--- src/regions/entities/region.entity.ts
+++ src/regions/entities/region.entity.ts
@@ -1,1 +1,13 @@
-export class Region {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Region {
+    @PrimaryGeneratedColumn('increment')
+    regionId: number;
+    
+    @Column('text')
+    regionName: string;
+
+    @Column('array')
+    regionStates: string[];
+}
